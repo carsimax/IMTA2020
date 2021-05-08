4ed266bf1e0e8f29dea8d234f0f5a452aa23d49e
carsimax <42248429+carsimax@users.noreply.github.com>
2021-05-08 11:21:01 -0500

    Hola

---
+++ aplicacion/vista/Untitled-1.php
@@ -0,0 +1,45 @@
+<div class="col-sm">
+    <div class="row">
+        <div class="col-sm">
+            <h4 class="text-center"><small>Bienvenido</small></h4>
+            <p align="justify">
+                La agricultura es una actividad económica vital para el desarrollo de un país que demanda
+                cada
+                vez más cantidades de alimentos para una población en constante crecimiento. La agricultura
+                mexicana está expuesta a factores adversos que incrementan el riesgo y vulnerabilidad a la
+                siniestralidad, entre los que más afectan al desarrollo de los cultivos y causan más
+                pérdidas están
+                los climáticos (variabilidad y cambio climático) y la disponibilidad de recursos hídricos.
+                Para
+                afrontar con éxito este desafío es indispensable contar con información que permita evaluar
+                el
+                comportamiento no sólo de la producción agrícola de un país y del uso de la tierra, sino
+                también
+                estimar el uso del agua para riego, analizar patrones de uso y niveles de eficiencia para
+                fijar metas
+                compartidas con los usuarios y mejorar la productividad por unidad de volumen usado y por
+                unidad de superficie de suelo.
+            </p>
+            <p align="justify">
+                Varias instituciones gubernamentales proporcionan información estadística, principalmente
+                tabular, de la producción agrícola y volumen de agua entregado en las zonas agrícolas bajo
+                riego a
+                nivel nacional. Dado sus distintos orígenes, esta información está dispersa y no tiene un
+                formato
+                homogéneo, lo que dificulta su utilización. Ante esta situación, se requiere de plataformas
+                de
+                comunicación digital que faciliten el acceso y análisis efectivo de la información
+                disponible,
+                haciendo uso de las más recientes tecnologías de información y comunicación y los sistemas
+                de
+                información geográfica para maximizar el aprovechamiento de los datos generados.
+            </p>
+            <p align="justify">
+                En este contexto, se presenta el Sistema de Información Sobre el Uso del Agua de Riego en la Agricultura Nacional (SISUAR), con el propósito de brindar un punto de acceso común a la información relacionada con el uso y explotación del recurso agua en la agricultura nacional de riego. El sistema proporciona información estadística de la producción agrícola de los Distritos y las Unidades de Riego, presas, aprovechamientos subterráneos para la explotación del agua destinada al uso agrícola, datos sobre la evapotranspiración de cultivos, volúmenes utilizados y otras fuentes de datos relacionadas. La intención es poner al alcance de los usuarios interesados una hereramienta de consulta con una interfaz amigable y con herramientas que facilitan el análisis y la extracción de información.
+            </p>
+        </div>
+        <div class="col" id="padre">
+            <img id="hijo" style="max-width:100%;width:auto;height:auto;" class="img-responsive" src="/imagenes/Inicio.jpg">
+        </div>
+    </div>
+</div>
