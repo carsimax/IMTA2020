f3caf52888183666fa77138f003ca7a0aa27b252
Jorge <jorgecalderon.dev@gmail.com>
2021-05-15 17:19:16 -0500

    Cards arregladas

--- aplicacion/vista/principal.php
+++ aplicacion/vista/principal.php
@@ -36,8 +36,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                                 <h5 class="my-0 font-weight-normal text-center"><a href="/aplicacion/vista/acuifero/acuiferos.php">Acuíferos</a></h5>
                             </div>
                             <a href="/aplicacion/vista/acuifero/acuiferos.php"><img class="img-fluid" src="/imagenes/acuifero.png" title="Disponibilidad de Acuíferos" alt="Disponibilidad de Acuíferos"></a>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta los valores de Disponibilidad Media Anual de Agua Subterránea, Descarga Natural Comprometida, Recarga Media Anual, Volumen de Extracción de Aguas Subterráneas y demás información de cada uno de los Estados y Municipios de la República Mexicana.</p>
+                                <a href="/aplicacion/vista/acuifero/acuiferos.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
@@ -48,8 +49,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                                 <h5 class="my-0 font-weight-normal text-center"><a href="/aplicacion/vista/presa/presas.php">Presas</a></h5>
                             </div>
                             <a href="/aplicacion/vista/presa/presas.php"><img class="img-fluid" src="/imagenes/presa.png" alt="Principales presas" title="Principales presas"></a>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta los Volúmenes de Almacenamiento y los Niveles de Aguas Máximas Ordinarias (NAMO) y Extraordniarias (NAME) las principales presas de la República Mexicana.</p>
+                                <a href="/aplicacion/vista/presa/presas.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
@@ -59,8 +61,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                                 <h5 class="my-0 font-weight-normal text-center"><a href="/aplicacion/vista/pozo/pozos.php">Pozos</a></h5>
                             </div>
                             <a href="/aplicacion/vista/pozo/pozos.php"><img class="img-fluid" src="/imagenes/pozo.png" alt="Títulos de conseción" title="Pozos"></a>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta los diferentes tipos de Pozos por medio de Títulos de Concesión para conocer los Volúmenes de Extracción de Aguas Nacionales, Número de Anexos de Aguas Subterráneas y demás información de cada uno de los Estados y Municipios de la República Mexicana.</p>
+                                <a href="/aplicacion/vista/pozo/pozos.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
@@ -70,8 +73,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                                 <h5 class="my-0 font-weight-normal text-center"><a href="/aplicacion/vista/agricola/agricola.php">Estadística Agrícola</a></h5>
                             </div>
                             <a href="/aplicacion/vista/agricola/agricola.php"><img class="img-fluid" src="/imagenes/agricultura.png" alt="Estadística agrícola" title="Estadística agrícola"></a>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta los Informes Estadísticos de Producción Agrícola, Superficies Regadas y Volúmenes de Agua Distribuidos en los Distritos de Riego y Temporal Tecnificado, así como sus históricos.</p>
+                                <a href="/aplicacion/vista/agricola/agricola.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
@@ -81,8 +85,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                                 <h5 class="my-0 font-weight-normal text-center"><a href="/aplicacion/vista/calidad_agua_superficial/calidad_agua_superficial.php">Calidad del Agua</a></h5>
                             </div>
                             <a href="/aplicacion/vista/calidad_agua_superficial/calidad_agua_superficial.php"><img class="img-fluid" src="/imagenes/calidad_agua.png" alt="Calidad de agua en México" title="Calidad de agua en México"></a>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta los diferentes indicadores de la calidad del agua como lo son: Demanda Bioquímica de Oxígeno a cinco días (DBO5), Demanda Química de Oxígeno (DQO), Sólidos Suspendidos Totales (SST), Coliformes Fecales (CF) recabados por los sitios de monitoreo operados por la Conagua en todo el país.</p>
+                                <a href="/aplicacion/vista/calidad_agua_superficial/calidad_agua_superficial.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
@@ -92,8 +97,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                                 <h5 class="my-0 font-weight-normal text-center"><a href="/aplicacion/vista/estaciones_hidrometricas/estaciones_hidrometricas.php">Estaciones Hidrométricas</a></h5>
                             </div>
                             <a href="/aplicacion/vista/estaciones_hidrometricas/estaciones_hidrometricas.php"><img class="img-fluid" src="/imagenes/hidrometrica.png" alt="Estaciones Hidrométricas" title="Estaciones Hidrométricas"></a>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta las diferentes estaciones hidrométricas distribuidas en todo el país.</p>
+                                <a href="/aplicacion/vista/estaciones_hidrometricas/estaciones_hidrometricas.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
@@ -103,8 +109,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                                 <h5 class="my-0 font-weight-normal text-center"><a href="/aplicacion/vista/estaciones_climatologicas/estaciones_climatologicas.php">Estaciones Climatológicas</a></h5>
                             </div>
                             <a href="/aplicacion/vista/estaciones_climatologicas/estaciones_climatologicas.php"><img class="img-fluid" src="/imagenes/climatologica.png" alt="Estaciones climatológicas" title="Estaciones Climatológicas"></a>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta las diferentes estaciones climatológicas distribuidas en todo el país.</p>
+                                <a href="/aplicacion/vista/estaciones_climatologicas/estaciones_climatologicas.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
@@ -116,8 +123,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                             <a href="/aplicacion/vista/indice_marginacion/indice_marginacion.php">
                                 <img class="img-fluid" src="/imagenes/marginacion.png" alt="Índice de Marginación" title="Índice de Marginación">
                             </a>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta el índice de marginación estatal y municipal de todo el país.</p>
+                                <a href="/aplicacion/vista/indice_marginacion/indice_marginacion.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
@@ -131,8 +139,9 @@ if (!defined(__DIR__ . '/plantillas/header.php')) {
                                     <img class="img-fluid" src="/imagenes/Inventario.png" alt="Inventario de Obras de los Distritos de Riego" title="Inventario de Obras de los Distritos de Riego">
                                 </a>
                             </div>
-                            <div class="card-body text-justify">
+                            <div class="card-body text-justify d-flex justify-content-between flex-column">
                                 <p class="font-weight-normal">Consulta el inventario de obras de infraestructura, obras de cabeza y sociedades de responsabilidad limitada de Distritos de Riego.</p>
+                                <a href="/aplicacion/vista/inventario/inventario.php" class="btn btn-block btn-primary">Consultar</a>
                             </div>
                         </div>
                     </div>
