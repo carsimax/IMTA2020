da276977f181e9fdd118434e1698194c85b595d4
carsimax <42248429+carsimax@users.noreply.github.com>
2021-05-15 10:14:48 -0500

    Awata

--- aplicacion/vista/acuifero/acuifero.js
+++ aplicacion/vista/acuifero/acuifero.js
@@ -348,7 +348,107 @@ async function mostrarDOrganismo(data) {
         title: "DMA (hm³)",
       },
     ],
-    buttons: btnAcuOC,
+    buttons: [
+      {
+        extend: "excelHtml5",
+        title: "Disponibilidad de acuíferos por Organismo de cuenca ",
+        className: "btn btn-gob btn-sm",
+        text: "Exportar Excel",
+        exportOptions: {
+          columns: [1, 2, 3, 4, 5, 6, 7, 8],
+        },
+      },
+      {
+        extend: "pdfHtml5",
+        title: "Consulta acuíferos por Organismo de cuenca",
+        className: "btn btn-gob btn-sm",
+        text: "Exportar PDF",
+        messageBottom: citas,
+        orientation: "portrait",
+        pageSize: "A4",
+        exportOptions: {
+          columns: [1, 2, 3, 4, 5, 6, 7, 8],
+        },
+        customize: function (doc) {
+          //Remove the title created by datatTables
+          doc.content.splice(0, 1);
+          //Create a date string that we use in the footer. Format is dd-mm-yyyy
+          var now = new Date();
+          var jsDate =
+            now.getDate() +
+            "-" +
+            (now.getMonth() + 1) +
+            "-" +
+            now.getFullYear();
+          // It's important to create enough space at the top for a header !!!
+          doc.pageMargins = [20, 70, 20, 50];
+          // Set the font size fot the entire document
+          doc.defaultStyle.fontSize = 10;
+          // Set the fontsize for the table header
+          doc.styles.tableHeader.fontSize = 10;
+          doc["header"] = function () {
+            return {
+              columns: [
+                {
+                  image: logo,
+                  width: 200,
+                },
+                {
+                  alignment: "left",
+                  //italics: true,
+                  text: "Disponibilidad de acuíferos por Organismo de cuenca",
+                  fontSize: 12.5,
+                  margin: [10, 5],
+                },
+                {
+                  alignment: "right",
+                  fontSize: 10,
+                  text: jsDate.toString(),
+                },
+              ],
+              margin: 20,
+            };
+          };
+          doc["footer"] = function (page, pages) {
+            return {
+              columns: [
+                {
+                  // This is the right column
+                  alignment: "center",
+                  text: [
+                    "Página ",
+                    { text: page.toString() },
+                    " de ",
+                    { text: pages.toString() },
+                  ],
+                },
+              ],
+              margin: [50, 0],
+            };
+          };
+          var objLayout = {};
+          objLayout["hLineWidth"] = function (i) {
+            return 0.5;
+          };
+          objLayout["vLineWidth"] = function (i) {
+            return 0.5;
+          };
+          objLayout["hLineColor"] = function (i) {
+            return "#aaaaaa";
+          };
+          objLayout["vLineColor"] = function (i) {
+            return "#aaaaaa";
+          };
+          objLayout["paddingLeft"] = function (i) {
+            return 4;
+          };
+          objLayout["paddingRight"] = function (i) {
+            return 4;
+          };
+          doc.content[0].layout = objLayout;
+        },
+      },
+    ],
     language: {
       url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
       decimal: ".",
@@ -524,7 +624,107 @@ async function mostrarDEstado() {
                 title: "DMA (hm³)",
               },
             ],
-            buttons: btnAcuEST,
+            buttons: [
+              {
+                extend: "excelHtml5",
+                title: "Disponibilidad de acuíferos por Estado",
+                className: "btn btn-gob btn-sm",
+                text: "Exportar Excel",
+                exportOptions: {
+                  columns: [1, 2, 3, 4, 5, 6, 7, 8],
+                },
+              },
+              {
+                extend: "pdfHtml5",
+                title: "Disponibilidad de acuíferos por Estado",
+                className: "btn btn-gob btn-sm",
+                text: "Exportar PDF",
+                messageBottom: citas,
+                orientation: "portrait",
+                pageSize: "A4",
+                exportOptions: {
+                  columns: [1, 2, 3, 4, 5, 6, 7, 8],
+                },
+                customize: function (doc) {
+                  //Remove the title created by datatTables
+                  doc.content.splice(0, 1);
+                  //Create a date string that we use in the footer. Format is dd-mm-yyyy
+                  var now = new Date();
+                  var jsDate =
+                    now.getDate() +
+                    "-" +
+                    (now.getMonth() + 1) +
+                    "-" +
+                    now.getFullYear();
+                  // It's important to create enough space at the top for a header !!!
+                  doc.pageMargins = [20, 70, 20, 50];
+                  // Set the font size fot the entire document
+                  doc.defaultStyle.fontSize = 10;
+                  // Set the fontsize for the table header
+                  doc.styles.tableHeader.fontSize = 10;
+                  doc["header"] = function () {
+                    return {
+                      columns: [
+                        {
+                          image: logo,
+                          width: 200,
+                        },
+                        {
+                          alignment: "left",
+                          //italics: true,
+                          text: "Disponibilidad de acuíferos por Estado",
+                          fontSize: 12.5,
+                          margin: [10, 5],
+                        },
+                        {
+                          alignment: "right",
+                          fontSize: 10,
+                          text: jsDate.toString(),
+                        },
+                      ],
+                      margin: 20,
+                    };
+                  };
+                  doc["footer"] = function (page, pages) {
+                    return {
+                      columns: [
+                        {
+                          // This is the right column
+                          alignment: "center",
+                          text: [
+                            "Página ",
+                            { text: page.toString() },
+                            " de ",
+                            { text: pages.toString() },
+                          ],
+                        },
+                      ],
+                      margin: [50, 0],
+                    };
+                  };
+                  var objLayout = {};
+                  objLayout["hLineWidth"] = function (i) {
+                    return 0.5;
+                  };
+                  objLayout["vLineWidth"] = function (i) {
+                    return 0.5;
+                  };
+                  objLayout["hLineColor"] = function (i) {
+                    return "#aaaaaa";
+                  };
+                  objLayout["vLineColor"] = function (i) {
+                    return "#aaaaaa";
+                  };
+                  objLayout["paddingLeft"] = function (i) {
+                    return 4;
+                  };
+                  objLayout["paddingRight"] = function (i) {
+                    return 4;
+                  };
+                  doc.content[0].layout = objLayout;
+                },
+              },
+            ],
             language: {
               url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
             },
@@ -656,7 +856,108 @@ async function mostrarDAcuifero() {
                 title: "DMA (hm³)",
               },
             ],
-            buttons: btnAcuMUN,
+            buttons: [
+              {
+                extend: "excelHtml5",
+                title: "Disponibilidad de acuíferos",
+                className: "btn btn-gob btn-sm",
+                text: "Exportar Excel",
+                exportOptions: {
+                  columns: [1, 2, 3, 4, 5, 6, 7, 8],
+                },
+              },
+              {
+                extend: "pdfHtml5",
+                title: "Disponibilidad de acuíferos",
+                className: "btn btn-gob btn-sm",
+                text: "Exportar PDF",
+                messageBottom: citas,
+                orientation: "portrait",
+                pageSize: "A4",
+                exportOptions: {
+                  columns: [1, 2, 3, 4, 5, 6, 7, 8],
+                },
+                customize: function (doc) {
+                  //Remove the title created by datatTables
+                  doc.content.splice(0, 1);
+                  //Create a date string that we use in the footer. Format is dd-mm-yyyy
+                  var now = new Date();
+                  var jsDate =
+                    now.getDate() +
+                    "-" +
+                    (now.getMonth() + 1) +
+                    "-" +
+                    now.getFullYear();
+
+                  // It's important to create enough space at the top for a header !!!
+                  doc.pageMargins = [20, 70, 20, 50];
+                  // Set the font size fot the entire document
+                  doc.defaultStyle.fontSize = 10;
+                  // Set the fontsize for the table header
+                  doc.styles.tableHeader.fontSize = 10;
+                  doc["header"] = function () {
+                    return {
+                      columns: [
+                        {
+                          image: logo,
+                          width: 200,
+                        },
+                        {
+                          alignment: "left",
+                          //italics: true,
+                          text: "Disponibilidad de acuíferos",
+                          fontSize: 12.5,
+                          margin: [10, 5],
+                        },
+                        {
+                          alignment: "right",
+                          fontSize: 10,
+                          text: jsDate.toString(),
+                        },
+                      ],
+                      margin: 20,
+                    };
+                  };
+                  doc["footer"] = function (page, pages) {
+                    return {
+                      columns: [
+                        {
+                          // This is the right column
+                          alignment: "center",
+                          text: [
+                            "Página ",
+                            { text: page.toString() },
+                            " de ",
+                            { text: pages.toString() },
+                          ],
+                        },
+                      ],
+                      margin: [50, 0],
+                    };
+                  };
+                  var objLayout = {};
+                  objLayout["hLineWidth"] = function (i) {
+                    return 0.5;
+                  };
+                  objLayout["vLineWidth"] = function (i) {
+                    return 0.5;
+                  };
+                  objLayout["hLineColor"] = function (i) {
+                    return "#aaaaaa";
+                  };
+                  objLayout["vLineColor"] = function (i) {
+                    return "#aaaaaa";
+                  };
+                  objLayout["paddingLeft"] = function (i) {
+                    return 4;
+                  };
+                  objLayout["paddingRight"] = function (i) {
+                    return 4;
+                  };
+                  doc.content[0].layout = objLayout;
+                },
+              },
+            ],
             language: {
               url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
             },
